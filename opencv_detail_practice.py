7487d95ec2a38bd6c4bc190240ddcb54d435105d
SamaySah21 <samaysah21@gmail.com>
2022-03-30 11:43:25 +0530

    image read

--- opencv_detail_practice.py
+++ opencv_detail_practice.py
@@ -10,10 +10,12 @@
 import cv2 as cv
 
                   # READ IMAGE
-img = cv.imread("gr_dog.jpg")
-cv.imshow("dog", img)
-cv.waitKey(0)   # 0 means show the image infintely untill any key is pressed
+def readImage():
+    img = cv.imread("gr_dog.jpg")
+    cv.imshow("dog", img)
+    cv.waitKey(0)   # 0 means show the image infintely untill any key is pressed
 
+# readImage()
                  # READ VIDEO
 # capture = cv2.VideoCapture(video_path)
 
@@ -23,5 +25,24 @@ cv.waitKey(0)   # 0 means show the image infintely untill any key is pressed
 
 #capture = cv2.VideoCapture(2) # 2 for second camera connected to computer
 
+# reading the video present in local
+
+def readVideo():
+    capture_video = cv.VideoCapture("cars.avi")
+
+    # video we can read frame by frame
+
+    while True:
+        isTrue, frame = capture_video.read()  # isTrue : whether frame is read or not
+
+        cv.imshow("carvideo", frame)
+
+        #  video will stop when any key is pressed and video rans for 20 ms
+        if cv.waitKey(20) and 0xFF == ord("d"):
+            break
+
+    capture_video.release()  # releasing the pointer
+    cv.destroyAllWindows()  # close all the windows
 
 
+readVideo()
